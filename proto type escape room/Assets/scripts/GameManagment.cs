d96cd3b1a002c85e633f9eee558f5e3724c727cd
Ryan Jones <ryan.jvjones@ntlworld.com>
2019-12-06 13:11:27 +0000

    lighinga nd pickup

---
+++ proto type escape room/Assets/scripts/GameManagment.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagment : MonoBehaviour
+{
+
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
---
+++ proto type escape room/Assets/scripts/Pickup1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup1 : MonoBehaviour
+{
+    Vector3 objectpos;
+    float distance;
+
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject TempParent;
+    public bool isHolding = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isHolding == true)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(TempParent.transform);
+
+        }
+        else
+        {
+            objectpos = item.transform.position;
+            item.transform.SetParent(null);
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.transform.position = objectpos;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isHolding = true;
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().detectCollisions = true;
+       
+
+    }
+
+    void OnMouseUp()
+    {
+        isHolding = false;
+     
+    }
+}
