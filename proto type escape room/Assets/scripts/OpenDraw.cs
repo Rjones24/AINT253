158db018b17d51e5e2cd074e4b483d947696b0ff
Ryan Jones <ryan.jones-24@students.plymouth.ac.uk>
2020-01-05 18:34:03 +0000

    mass asset input

---
+++ proto type escape room/Assets/scripts/OpenDraw.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDraw : MonoBehaviour
+{
+   
+    public bool onTrigger;
+    public bool DrawOpen;
+   
+
+    public Transform Draw;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+    private void Update()
+    {
+        if (DrawOpen)
+        {
+            
+            Draw.Translate(0.0f, 10.0f, 0f);
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (!DrawOpen)
+        {
+            if (onTrigger)
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press 'E' to open draw");
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    DrawOpen = true;
+                }
+            }
+
+        }
+    }
+}
---
+++ proto type escape room/Assets/scripts/ToSmall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToSmall : MonoBehaviour
+{
+    public bool onTrigger;
+
+
+    public Transform doorHinge;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onTrigger = false;
+    }
+
+
+
+    private void OnGUI()
+    {
+
+        if (onTrigger)
+        {
+            GUI.Box(new Rect(50, 250, 500, 25), "you are to small to reach the keypad try and find a step do that you can reach it");
+           
+        }
+    }
+}
